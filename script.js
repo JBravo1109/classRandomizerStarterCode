6965c5f23bcd03f886ceadb7a0e8d7ce069e373c
Joel Bravo <jbravo@aclanyc.com>
2017-05-04 04:17:42 +0000

    randomizer

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-
-
-
+var studentList=['Alan', 'Yanella', 'Josue', 'Joel', 'Tushar', 'Odalys'];
+var scriptEducators=['Aaron', 'Julia','Justin','Zach','Alyxe'];
+$( "#studentButton").click(function(){
+    var randomStudents = Math.floor (Math.random()*studentList.length);
+    $("#studentDisplay").append(ScriptEders[randomStudents]);
+});
+$("teacherButton").click(function(){
+    var randomTeachers=Math.floor(Math.random()*ScriptEducators.length);
+    $("teacherDisplay").append(ScriptEducators[randomTeachers]);
 });
